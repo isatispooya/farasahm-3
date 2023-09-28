306c44fec935c909a47a4837531856ca3e197060
moeen dehqan <moeen.dehqan@gmail.com>
2023-09-28 13:55:01 +0330

    s

--- pipeData.py
+++ pipeData.py
@@ -15,21 +15,24 @@ while True:
             except:
                 print('Web Crawling Break')
                 time.sleep(60)
+    try:
+        if Fnc.is_time_between(8,17) and Fnc.is_time_divisible(15):
+        # این بخش حلقه برای دریافت اطلاعات از کار گزاری است
+            Fnc.getTseDate()
+        # این بخش حلقه برای دریافت اطلاعات از کار گزاری است
+            ApiMethods.GetAllTradeInDate()
+            desk_broker_volumeTrade_cal()
+            desk_broker_turnover_cal()
+        
+        if Fnc.is_time_between(17,23):
+            Fnc.getTse30LastDay()
+            ApiMethods.GetAllTradeLastDate()
 
-    if Fnc.is_time_between(8,17) and Fnc.is_time_divisible(15):
-    # این بخش حلقه برای دریافت اطلاعات از کار گزاری است
-        Fnc.getTseDate()
-    # این بخش حلقه برای دریافت اطلاعات از کار گزاری است
-        ApiMethods.GetAllTradeInDate()
-        desk_broker_volumeTrade_cal()
-        desk_broker_turnover_cal()
-    
-    if Fnc.is_time_between(17,23):
-        Fnc.getTse30LastDay()
-        ApiMethods.GetAllTradeLastDate()
-
-    time.sleep(60)
-    print('End of circle')
+        time.sleep(60)
+        print('End of circle')
+    except:
+        time.sleep(60)
+        print('crash')
     
 
 
