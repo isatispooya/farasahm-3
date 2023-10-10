f58dccd62457a4e881a6fac303cb84885681dbeb
moeen dehqan <moeen.dehqan@gmail.com>
2023-10-10 11:25:52 +0330

    ...

--- Login.py
+++ Login.py
@@ -7,13 +7,13 @@ import base64
 import random
 import numpy as np
 import string
-
-client = pymongo.MongoClient()
-farasahmDb = client['farasahm2']
-
 import json
 import requests
 import random
+import GuardPyCaptcha
+key = 'KPms1b_Kibq5XR6M0d88rJTsjjgdlBFzbFN4irIxiHo='
+client = pymongo.MongoClient()
+farasahmDb = client['farasahm2']
 
 frm ='30001526'
 usrnm = 'isatispooya'
@@ -38,13 +38,13 @@ def VerificationPhone(phone):
 def captchaGenerate():
     font = cv2.FONT_HERSHEY_COMPLEX
     captcha = np.zeros((50,250,3), np.uint8)
-    captcha[:] = (234, 228, 228)#(random.randint(235,255),random.randint(245,255),random.randint(245,255))
+    captcha[:] = (234, 228, 228)
     font= cv2.FONT_HERSHEY_SIMPLEX
     texcode = ''
     listCharector =  string.digits+string.ascii_lowercase+string.digits
     for i in range(1,5):
         bottomLeftCornerOfText = (random.randint(35,45)*i,35+(random.randint(-8,8)))
-        fontScale= random.randint(7,15)/10
+        fontScale= random.randint(8,16)/10
         fontColor= (random.randint(0,180),random.randint(0,180),random.randint(0,180))
         thickness= random.randint(1,2)
         lineType= 1
@@ -56,14 +56,11 @@ def captchaGenerate():
             pt2 = (random.randint(0,250),random.randint(0,50))
             lineColor = (random.randint(0,150),random.randint(0,150),random.randint(0,150))
             cv2.line(captcha,pt1,pt2,lineColor,1)
-    address = 'C:\\Users\\moeen\\Desktop\\project\\pishkar\\Front\\pishkar\\public\\captcha\\'+texcode+'.jpg'
     stringImg = base64.b64encode(cv2.imencode('.jpg', captcha)[1]).decode()
     return [texcode,stringImg]
 
-from cryptography.fernet import Fernet
 
 
-key = 'KPms1b_Kibq5XR6M0d88rJTsjjgdlBFzbFN4irIxiHo='
 f = Fernet(key)
 
 def encrypt(msg):
@@ -162,7 +159,4 @@ def getApp(data):
                             item['sub'] = appSubMenu
                             appMenu.append(item)
             app['menu'] = appMenu
-
-
-
     return json.dumps({'reply':True, 'app':app})
