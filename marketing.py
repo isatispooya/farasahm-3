aab0a12f87d768f3ac29595e373e5cddcfe7f69f
Mahya Abouei <mahyaaboueii@gmail.com>
2024-07-30 11:22:03 +0330

    marketing

--- marketing.py
+++ marketing.py
@@ -2,8 +2,8 @@ from setting import farasahmDb
 import pandas as pd
 from bson import ObjectId
 import json
-
-
+from persiantools.jdatetime import JalaliDate
+from datetime import datetime
 
 def clean_list(data):
     cleaned_data = []
@@ -11,6 +11,20 @@ def clean_list(data):
         if isinstance(i, str) and i != 'nan' and not i.isdigit() and i != "":
             cleaned_data.append(i)
     return cleaned_data
+
+def national_id (national_id , code) :
+    if isinstance(national_id, str):
+        for i in code:
+            if i in national_id:
+                return True
+    return False
+
+def filter_date_symbol(df):
+    df['date'] = df['date'].fillna(0)
+    df['date'] = df['date'].apply(int)
+    df = df[df['date']==df['date'].max()]
+    df = df.reset_index()
+    return df
 
 
 #  registerNoBours لیست شهرهای فایل 
@@ -46,6 +60,7 @@ def symbol_nobours (data) :
 
 
 
+
 def fillter_registernobours (config) :
     print(config)
     if not config['enabled'] : 
@@ -54,9 +69,50 @@ def fillter_registernobours (config) :
         df = farasahmDb['registerNoBours'].find({'symbol':{'$in':config['symbol']}})
     else :
         df = farasahmDb['registerNoBours'].find({})
-        
     df = pd.DataFrame(df)
-    print(df)
+    df = df.groupby(by='symbol').apply(filter_date_symbol)
+    if len (config ['city']) >0 :
+        df = df [df['صادره'].isin(config['city'])]
+
+    if len (config['national_id']) >0 :
+        df = df[df['کد ملی'].apply(lambda x:national_id(x, config['national_id']))]       
+
+    if config['birthday']['from'] :
+        from_date = JalaliDate.fromtimestamp(int(config['birthday']['from']/1000))
+        from_date = int(str(from_date).replace('-',''))
+        df['تاریخ تولد'] = df['تاریخ تولد'].fillna(0)
+        df['تاریخ تولد'] = [str(x).replace('/','') for x in df['تاریخ تولد']]
+        df['تاریخ تولد'] = df['تاریخ تولد'].apply(int)
+        df = df[df['تاریخ تولد']>=from_date]
+
+        to_date = JalaliDate.fromtimestamp(int(config['birthday']['to']/1000))
+        to_date = int(str(to_date).replace('-',''))
+        df['تاریخ تولد'] = df['تاریخ تولد'].fillna(0)
+        df['تاریخ تولد'] = [str(x).replace('/','') for x in df['تاریخ تولد']]
+        df['تاریخ تولد'] = df['تاریخ تولد'].apply(int)
+        df = df[df['تاریخ تولد']<=to_date]
+
+
+    if config['amount']['from'] :
+        df['تعداد سهام'] = df['تعداد سهام'].fillna(0)
+        df['تعداد سهام'] = df['تعداد سهام'].astype(int)
+        from_amount = int(config['amount']['from'])
+        df = df[df['تعداد سهام'] >= from_amount]
+        to_amount = int(config['amount']['to'])
+        df = df[df['تعداد سهام'] <= to_amount]
+       
+
+
+
+
+    
+        print(df)
+
+
+
+
+
+    # print(df)
     return []
 
 
