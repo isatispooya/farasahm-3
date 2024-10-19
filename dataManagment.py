6fb70771d53d1425fb11b90b9c7843fed3af9947
moeendehqan <moeen.dehqan@gmail.com>
2024-10-19 09:06:16 +0330

    .

--- dataManagment.py
+++ dataManagment.py
@@ -31,10 +31,9 @@ def Update(access,daily,registerdaily):
     dfRegister = zipToDf(registerdaily)
     if len(dfDaily)==0: return json.dumps({'replay':False,'msg':'فایل معاملات خالی است'})
     if len(dfRegister)==0: return json.dumps({'replay':False,'msg':'فایل رجیستر خالی است'})
-    dfDaily = dfDaily.rename(columns={
-        'کد ملی خریدار':'کد/شناسه ملی خریدار',
-        'کد ملی فروشنده':'کد/شناسه ملی فروشنده',
-        })
+    for i in [{'old':'کد ملی خریدار','new':'کد/شناسه ملی خریدار'},{'old':'کد ملی فروشنده','new':'کد/شناسه ملی فروشنده'}]:
+        dfDaily = dfDaily.rename(columns={i['new']:i['old']})
+        dfRegister = dfRegister.rename(columns={i['new']:i['old']})
     if 'نماد کدال' not in dfRegister.columns:
         dfRegister['نماد کدال'] = [str(x).replace('ذویسات','ویسا').replace('ذبازرگا','بازرگام') for x in dfRegister['نماد']]
     if 'نماد کدال' not in dfDaily.columns:
--- duplicateTradeListBroker.py
+++ duplicateTradeListBroker.py
@@ -1,7 +1,85 @@
-from setting import farasahmDb
+from setting import farasahmDb, pishkarDb
 import pandas as pd
+from Login import SendSms
+# import random
+# def ch(num):
+#     num = str(num)
+#     if len(num) < 10:
+#         return False
+#     try:
+#         num = int(num)
+#     except:
+#         return False
+#     num = '0'+str(num)
+#     if len(num) != 11:
+#         return False
+#     return num
+# df_reg = pd.DataFrame(farasahmDb['registerNoBours'].find({},{'شماره تماس':1}))
+# df_bor = pd.DataFrame(farasahmDb['customerofbroker'].find({},{'Mobile':1}))
+# df_ins = pd.DataFrame(pishkarDb['customers'].find({},{'تلفن همراه':1}))
 
-df = pd.DataFrame(farasahmDb['registerNoBours'].find({'symbol':'devisa'}))
-df = df[df['date'] == df['date'].max()]
+# df_reg = df_reg.rename(columns={'شماره تماس':'mobile'})
+# df_bor = df_bor.rename(columns={'Mobile':'mobile'})
+# df_ins = df_ins.rename(columns={'تلفن همراه':'mobile'})
 
-print(df['date'].max())
+# df = pd.concat([df_reg,df_bor,df_ins])
+# df = df.dropna()
+# df['mobile'] = df['mobile'].apply(ch)
+# df = df[df['mobile']!=False]
+# df = df.drop_duplicates(subset=['mobile'])
+# df.to_excel('df.xlsx')
+
+import pandas as pd
+from concurrent.futures import ThreadPoolExecutor
+
+txt = 'مشتری گرامی ایساتیس پویا\nفرصت ویژه سرمایه‌گذاری با سود سالانه 43 درصد در سکوی تأمین مالی ایساتیس کراد\nisatiscrowd.ir'
+
+def send_sms_to_mobile(mobile):
+    SendSms(mobile, txt)
+
+df = pd.read_excel('d351.xlsx', dtype=str)
+# df['sss'] = ['isatiscrowd.ir' in x for x in df['پیش شماره']]
+# df['mobile'] = ['0' + str(x) for x in df['گیرنده']]
+# df =df[df['sss']==True]
+# print(df)
+# dff = pd.read_excel('df.xlsx', dtype=str)
+# dff['kk'] = [x not in df['mobile'].to_list()  for x in dff['mobile']]
+# dff = dff[dff['kk'] == True]
+# dff.to_excel('df.xlsx')
+with ThreadPoolExecutor(max_workers=10) as executor:
+    executor.map(send_sms_to_mobile, df['mobile'])
+    
+
+
+
+# d351 = []
+# for i in range(0,9999):
+#     print(i)
+#     n = str(i)
+#     if len(n)<4:
+#         ziro = (4 - len(n))*'0'
+#         n = ziro + n
+#     num = '0913151'+n
+#     if num not in df:
+#         d351.append(num)
+#     num = '0913152'+n
+#     if num not in df:
+#         d351.append(num)
+#     num = '0913153'+n
+#     if num not in df:
+#         d351.append(num)
+#     num = '0913154'+n
+#     if num not in df:
+#         d351.append(num)
+
+
+# df_351 = pd.DataFrame(d351, columns=['mobile']) 
+# df_351.to_excel('d351.xlsx', index=False)
+
+# print(len(d351))
+
+
+
+
+
+# print(len(d351))
